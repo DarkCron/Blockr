15c2b9e528d86dd0a04be32e3ea1966bf8383ab7
Tim Vuegen <tim.vuegen@hotmail.com>
2020-03-09 15:39:36 +0100

    Add pipelinr dependency Add HandlerBase Add GetWorldHandler

---
+++ src/com/blockr/domain/handlers/HandlerBase.java
@@ -0,0 +1,20 @@
+package com.blockr.domain.handlers;
+
+import an.awesome.pipelinr.Command;
+import com.blockr.domain.State;
+
+import java.util.logging.Handler;
+
+public abstract class HandlerBase<C extends Command<R>, R> implements Command.Handler<C, R> {
+
+    protected State getState(){
+        return state;
+    }
+
+    private final State state;
+
+    public HandlerBase(State state){
+        this.state = state;
+    }
+
+}
---
+++ src/com/blockr/domain/handlers/getworld/GetWorld.java
@@ -0,0 +1,8 @@
+package com.blockr.domain.handlers.getworld;
+
+import an.awesome.pipelinr.Command;
+import com.blockr.domain.gameworld.ReadOnlyGameWorld;
+
+public class GetWorld implements Command<ReadOnlyGameWorld> {
+
+}
