0fe1b8846bcc9eede894ecd776dcba471df0c26c
Tim Vuegen <tim.vuegen@hotmail.com>
2020-04-22 14:37:02 +0200

    Initialize gameworld via args

--- Blockr/src/com/blockr/main/Main.java
+++ Blockr/src/com/blockr/main/Main.java
@@ -1,4 +1,5 @@
 package com.blockr.main;
+
 import an.awesome.pipelinr.Pipeline;
 import an.awesome.pipelinr.Pipelinr;
 import com.blockr.State;
@@ -6,10 +7,9 @@ import com.blockr.handlers.actions.ActionHandler;
 import com.blockr.handlers.actions.redo.DoRedoHandler;
 import com.blockr.handlers.actions.reset.DoResetHandler;
 import com.blockr.handlers.actions.undo.DoUndoHandler;
+import com.blockr.handlers.blockprogram.addblock.AddBlockHandler;
 import com.blockr.handlers.blockprogram.canstart.CanStartHandler;
-import com.blockr.handlers.blockprogram.connectconditionblock.ConnectConditionBlock;
 import com.blockr.handlers.blockprogram.connectconditionblock.ConnectConditionBlockHandler;
-import com.blockr.handlers.blockprogram.connectcontrolflowbody.ConnectControlFlowBody;
 import com.blockr.handlers.blockprogram.executeprogram.ExecuteProgramHandler;
 import com.blockr.handlers.blockprogram.getblockprogram.GetBlockProgramHandler;
 import com.blockr.handlers.blockprogram.getrootblock.GetRootBlockHandler;
@@ -22,51 +22,58 @@ import com.blockr.handlers.ui.input.recordMousePos.GetMouseRecordHandler;
 import com.blockr.handlers.ui.input.recordMousePos.SetRecordMouseHandler;
 import com.blockr.handlers.ui.input.resetuistate.ResetUIStateHandler;
 import com.blockr.handlers.world.GetWorldHandler;
-import com.blockr.handlers.blockprogram.addblock.AddBlockHandler;
-import com.kul.CanvasWindow;
+import com.gameworld.GameWorld;
 import com.ui.MyCanvasWindow;
-import com.ui.components.divcomponent.DivComponent;
 
 import javax.swing.*;
-import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
 import java.util.stream.Stream;
 
 public class Main {
 
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 
+        if(args.length == 0){
+            System.out.println("The gameworld classname must be provided");
+            return;
+        }
 
-    private static final State state = new State(null);
+        var className = args[0];
+        var gameWorld = (GameWorld)Class.forName(className).getConstructor().newInstance();
 
-    private static final Pipeline pipeline = new Pipelinr()
-            .with(() -> Stream.of(
-                      new GetWorldHandler(state)
-                    , new CanStartHandler(state)
-                    , new SetPaletteSelectionHandler(state)
-                    , new GetPaletteSelectionHandler(state)
-                    , new SetProgramSelectionHandler(state)
-                    , new GetProgramSelectionHandler(state)
-                    , new GetRootBlockHandler(state)
-                    , new AddBlockHandler(state)
-                    , new GetMouseRecordHandler(state)
-                    , new SetRecordMouseHandler(state)
-                    , new ExecuteProgramHandler(state)
-                    , new GetBlockProgramHandler(state)
-                    , new RemoveBlockHandler(state)
-                    , new ResetUIStateHandler(state)
-                    , new ConnectConditionBlockHandler(state)
-                    , new DoRedoHandler(state)
-                    , new DoUndoHandler(state)
-                    , new DoResetHandler(state)
+        var state = new State(gameWorld);
+
+        SwingUtilities.invokeLater(() -> new MyCanvasWindow("test", BlockrUi.build(buildPipeline(state))));
+    }
+
+    private static Pipeline buildPipeline(State state){
+        return new Pipelinr()
+                .with(() -> Stream.of(
+                        new GetWorldHandler(state)
+                        , new CanStartHandler(state)
+                        , new SetPaletteSelectionHandler(state)
+                        , new GetPaletteSelectionHandler(state)
+                        , new SetProgramSelectionHandler(state)
+                        , new GetProgramSelectionHandler(state)
+                        , new GetRootBlockHandler(state)
+                        , new AddBlockHandler(state)
+                        , new GetMouseRecordHandler(state)
+                        , new SetRecordMouseHandler(state)
+                        , new ExecuteProgramHandler(state)
+                        , new GetBlockProgramHandler(state)
+                        , new RemoveBlockHandler(state)
+                        , new ResetUIStateHandler(state)
+                        , new ConnectConditionBlockHandler(state)
+                        , new DoRedoHandler(state)
+                        , new DoUndoHandler(state)
+                        , new DoResetHandler(state)
+                        )
                 )
-            )
-            .with(
-                    () -> Stream.of(
-                            new ActionHandler(state)
-                    )
+                .with(
+                        () -> Stream.of(
+                                new ActionHandler(state)
+                        )
                 )
-            ;
-
-    public static void main(String[] args){
-        SwingUtilities.invokeLater(() -> new MyCanvasWindow("test", BlockrUi.build(pipeline)));
+                ;
     }
 }
